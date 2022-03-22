fd5ea4732559fbe95d876b397605f44afe4b7a90
Mateusz Pocztowski <mateusz.pocztowski@summer.agency>
2022-03-22 22:28:15 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,35 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int lowerScoreStudents = 0;
+            for (int i = 0; i < Students.Count; i++)
+            {
+                if (averageGrade > Students[i].AverageGrade)
+                {
+                    lowerScoreStudents++;
+                }
+            }
+
+            double twentyPercent = Students.Count / 5.0;
+
+            if (lowerScoreStudents >= twentyPercent * 4)
+                return 'A';
+            else if (lowerScoreStudents >= twentyPercent * 3)
+                return 'B';
+            else if (lowerScoreStudents >= twentyPercent * 2)
+                return 'C';
+            else if (lowerScoreStudents >= twentyPercent)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
